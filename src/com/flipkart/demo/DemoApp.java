2905a9a56ea1af99611235842d44aa288e462cf3
shikha <srai61778@gmail.com>
2023-09-12 16:33:27 +0530

    initial commit

---
+++ src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,10 @@
+package com.flipkart.demo;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("STS first repo");
+	}
+
+}
